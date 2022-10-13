105a73e37b44710e13ad43b26dd6be601adcb678
JPenaloza1 <100963782+JPenaloza1@users.noreply.github.com>
2022-10-12 23:03:54 -0500

    Apuntes 12/10/22

---
+++ HolaMundoTypeScript/funciones.js
@@ -0,0 +1,15 @@
+"use strict";
+var saludo = "Hola mundo desde TypeScript";
+saludo = "Cambio";
+var numero;
+numero = 10;
+var cualquiera;
+cualquiera = "cadena";
+cualquiera = 10;
+var PI_NUMERO = 3.14;
+function saludar() {
+    console.log(saludo);
+    console.log(numero);
+    console.log(PI_NUMERO);
+}
+saludar();
---
+++ HolaMundoTypeScript/funciones.ts
@@ -0,0 +1,20 @@
+let saludo = "Hola mundo desde TypeScript";
+
+saludo = "Cambio";
+
+let numero:number;
+numero = 10;
+
+let cualquiera: any;
+cualquiera = "cadena";
+cualquiera = 10;
+
+const PI_NUMERO = 3.14;
+
+function saludar() {
+    console.log(saludo);
+    console.log(numero);
+    console.log(PI_NUMERO);
+}
+
+saludar();
