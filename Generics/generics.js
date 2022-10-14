9bf3b120a843212bb3aee16ea116124ef18245e7
JPenaloza1 <100963782+JPenaloza1@users.noreply.github.com>
2022-10-13 23:40:59 -0500

    Apuntes 13/10/22

---
+++ Generics/generics.js
@@ -0,0 +1,6 @@
+"use strict";
+let arregloNumeros;
+arregloNumeros = [1, 2, 3, 4];
+// arregloNumeros = ["Juan", "Carlos"];
+console.log(arregloNumeros);
+console.log(arregloNumeros[0]);
---
+++ Generics/generics.ts
@@ -0,0 +1,7 @@
+let arregloNumeros:Array<number>;
+
+arregloNumeros = [1, 2, 3, 4];
+// arregloNumeros = ["Juan", "Carlos"];
+
+console.log(arregloNumeros);
+console.log(arregloNumeros[0]);
